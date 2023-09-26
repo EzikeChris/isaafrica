33d166c5a88eb8fa76a30ec0f43a07afccf6e0e3
ChrisEzikeJL <chris@joint.cool>
2023-09-26 10:11:53 +0100

    feat: create homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <div className="home">
+        {" "}
+        <h1 id="home-heading">
+          Get ahead, Stay Ahead
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
