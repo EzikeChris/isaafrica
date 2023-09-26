81316a7562ae54ab3deccbc5568ada74a66fa64f
ChrisEzikeJL <chris@joint.cool>
2023-09-26 10:24:27 +0100

    feat: update index file

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import "./styles/Calendar.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CoursesPage from "./pages/CoursesPage";
+import SchedulePage from "./pages/SchedulePage";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Navbar from "./components/Navbar";
+
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "schedule",
+        element: <SchedulePage />,
+      },
+      {
+        path: "courses",
+        element: <CoursesPage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
